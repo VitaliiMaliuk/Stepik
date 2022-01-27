3b3133ace0ae4b453ed43b932c833570ad9bf374
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-01-27 16:20:24 +0200

    one folder

---
+++ Input-output data/Integer arithmetic. Part 1/2.4.7_Arithmetic operations.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = a + b
+d = a - b
+e = a * b
+print(str(a), '+', str(b), '=', c)
+print(str(a), '-', str(b), '=', d)
+print(str(a), '*', str(b), '=', e)
---
+++ Input-output data/Integer arithmetic. Part 1/2.4.8_Arithmetic progression.py
@@ -0,0 +1,5 @@
+a = int(input())
+d = int(input())
+n = int(input())
+an = a + d * (n - 1)
+print(an)
