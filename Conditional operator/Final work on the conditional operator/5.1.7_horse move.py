1eb31d4eae975fcc6c7f99a0a4e8f24f9923d0d8
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-01-28 12:01:31 +0200

    Даны две различные клетки шахматной доски. Напишите программу,  которая определяет, может ли конь попасть с первой клетки на вторую одним ходом. Программа получает на вход четыре числа от 1 до 8 каждое, задающие номер столбца и номер строки сначала для первой клетки, потом для второй клетки. Программа должна вывести «YES», если из первой клетки ходом коня можно попасть во вторую или «NO» в противном случае.

--- Conditional operator/Final work on the conditional operator/5.1.7_horse move.py
+++ Conditional operator/Final work on the conditional operator/5.1.7_horse move.py
@@ -3,8 +3,9 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 if (x1 - 1 == x2 or x1 + 1 == x2) and (y1 - 2 == y2 or y1 + 2 == y2):
-    print ('YES')
+    print('YES')
 elif (x1 - 2 == x2 or x1 + 2 == x2) and (y1 - 1 == y2 or y1 + 1 == y2):
-    print ('YES')
+    print('YES')
 else:
-    print ('NO')
+    print('NO')
+    
