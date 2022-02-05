fb369be005b525549cda30dc76acbbb741a10b22
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-02-05 16:47:20 +0200

    На вход программе подается одна строка. Напишите программу, которая выводит сообщение «Цифра» (без кавычек), если строка содержит цифру. В противном случае вывести сообщение «Цифр нет» (без кавычек).

---
+++ String data type/Indexing/9.1.7_digit_2.py
@@ -0,0 +1,5 @@
+s = input()
+if '0' in s or '1' in s or '2' in s or '3' in s or '4' in s or '5' in s or '6' in s or '7' in s or '8' in s or '9' in s:
+    print('Цифра')
+else:
+    print('Цифр нет')
