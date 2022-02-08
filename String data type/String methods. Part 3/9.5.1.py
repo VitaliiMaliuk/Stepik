718714a10e7180eb24d58f717d8d45944860276c
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-02-08 12:37:00 +0200

    Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст: «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).

---
+++ String data type/String methods. Part 3/9.5.1.py
@@ -0,0 +1,6 @@
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+year = '2010'
+quantity = '10k'
+currency = 'Bitcoin'
+txt = 'In {}, someone paid {} {} for two pizzas.'.format(year, quantity, currency)
+print(txt)
